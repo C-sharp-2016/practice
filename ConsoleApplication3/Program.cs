d0f2cc88f1925d11622a4dddc6b67f78e4927a4a
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-20 08:12:53 +0800

    int variable, string convert to int32, readline, press any key or readkey

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -10,7 +10,27 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         { 
-           
+            // Declare variables
+            int num1;
+            int num2;
+
+            // Ask to input new integer
+            Console.WriteLine("Type a number to be multiplied: ");
+
+            // Read the user input and convert from string to integer 32 bit
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask to input second integer
+            Console.WriteLine("Type another number: ");
+
+            // Read the user input and convert from string to integer 32 bit
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Mutiply the 2 integer inputed
+            Console.WriteLine("The result is :" + num1 * num2);  
+
+            // Wait to press any key before console quit
+            Console.ReadKey();
         }
     }
 }
