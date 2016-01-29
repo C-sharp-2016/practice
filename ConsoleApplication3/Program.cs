bf87ca9cbff81221d6179c1066101f8c7b4adfce
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-30 00:33:20 +0800

    Structs

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -8,11 +8,37 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication3
 {
+    struct SimpleStruct
+    {
+        private int xval;
+        public int X
+        {
+            get
+            {
+                return xval;
+            }
+            set
+            {
+                if (value < 100)
+                    xval = value;
+            }
+        }
+        public void DisplayX()
+        {
+            Console.WriteLine("The stored value is: {0}", xval);
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-
+            SimpleStruct ss = new SimpleStruct();
+            ss.X = 5;
+            ss.DisplayX();
         }
-    } 
+    }
+
+    
 }
