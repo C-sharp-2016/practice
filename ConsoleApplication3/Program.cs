6175336eeeeaa5b9ad2b2286cca816b422641a1f
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-30 00:24:02 +0800

    Interface

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -10,9 +10,54 @@ namespace ConsoleApplication3
 {
     class Program
     { 
+
+        interface Iitem
+        {
+            string name { get; set; }
+            int goldVal { get; set; }
+
+            void Equip();
+            void Sell();  
+
+        }
+
+        interface IDamage
+        {
+            int damage { get; set; }
+            string attakerName { get; set; } 
+        }
+
+
+        class Sword : Iitem, IDamage
+        {
+            public string name { get; set; }
+            public int goldVal { get; set; }
+
+            public int damage { get; set; }
+            public string attakerName { get; set; }
+
+            public void Equip()
+            {
+                Console.WriteLine(name + " Equip this sword");
+                Console.WriteLine(" The {0} sword was damaged", name);
+            }
+            public void Sell()
+            {
+                Console.WriteLine(name + "Sell this sword");
+            }   
+        }
+         
         static void Main(string[] args)
         {
-           
+
+            Sword sword = new Sword();
+
+            sword.name = "danger";
+            sword.attakerName = "Tiger";
+            sword.damage = 1000;
+
+            sword.Equip();
+            sword.Sell();
         } 
     } 
 }
