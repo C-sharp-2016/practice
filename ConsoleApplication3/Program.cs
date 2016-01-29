67b19370097da7f4183659be7aec3f9b38a936a8
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-30 00:03:00 +0800

    Clean file

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -9,14 +9,10 @@ using System.Threading.Tasks;
 namespace ConsoleApplication3
 {
     class Program
-    {
-        delegate int CountIt(int x);
-
+    { 
         static void Main(string[] args)
         {
-            CountIt testDel = (int x) => x + 5;
-            int result = testDel(5);
-            Console.WriteLine(result);
+           
         } 
     } 
 }
